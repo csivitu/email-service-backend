5abaf30fd73bb6da473eea5dcf4fa094a49a1424
ashikka <ashikagupta28@gmail.com>
2020-11-14 01:10:43 +0530

    feat: Added email script

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 let { ServiceBroker } = require("moleculer");
 let ApiService = require("moleculer-web");
+require('dotenv').config()
 
 let broker = new ServiceBroker({ logger: console });
 
 broker.createService({
-    mixins: [ApiService],
-    name: "greet",
-    actions: {
-        helloWorld() {
-            return 'Hello, world!';
-        }
-    }
+  mixins: [ApiService],
+  name: "greet",
+  actions: {
+    send() {
+      var API_KEY = process.env.API_KEY;
+      var DOMAIN = process.env.DOMAIN;
+        console.log(DOMAIN);
+      var mailgun = require("mailgun-js")({
+        apiKey: API_KEY,
+        domain: DOMAIN,
+        host: process.env.HOST,
+      });
+
+      const data = {
+        from: process.env.SENDER_EMAIL,
+        to: "test@gmail.com",
+        subject: "Hello",
+        text: "Testing some Mailgun awesomeness!",
+      };
+
+      mailgun.messages().send(data, (error, body) => {
+        console.log(error);
+        console.log(body);
+      });
+      return("Email sent");
+    },
+  },
 });
 
 broker.start();
