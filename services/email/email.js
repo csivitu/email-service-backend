c3750e8bb27fa0d51fa12f2b5c86fe26f2113b32
alias-rahil <rahil.kabani.4@gmail.com>
2020-11-18 03:19:01 +0530

    feat: error handling if any api key not present

--- services/email/email.js
+++ services/email/email.js
@@ -7,14 +7,19 @@ broker.createService({
   name: 'email',
   actions: {
     send (ctx) {
-      let response = ''
       if (ctx.params.auth !== auth) {
         ctx.meta.$statusCode = 401
-        return { error: 'Error: auth key invalid' }
+        return { error: 'Error: auth key is invalid' }
       }
 
+      if (!process.env.MAILGUN_API_KEY && !process.env.SENGRID_API_KEY) {
+        ctx.meta.$statusCode = 503
+        return { error: 'Error: no api key is available righ now, please try again later' }
+      }
+
+      let response = ''
       try {
-        if (Math.floor(Math.random() * 2) % 2) {
+        if (Math.floor(Math.random() * 2) % 2 && process.env.SENGRID_API_KEY) {
           response = senGrid.sengrid(ctx)
         } else {
           response = mailGun.mailgun(ctx)
--- services/init.js
+++ services/init.js
@@ -12,7 +12,7 @@ broker.createService({
   settings: {
     port: process.env.PORT || '5001',
     cors: {
-      origin: ALLOWED_ORIGINS ? ALLOWED_ORIGINS.split(', ') : '*'
+      origin: ALLOWED_ORIGINS ? ALLOWED_ORIGINS.split(', ') : ''
     }
   }
 })
