2589d0000028ac18cd3acee43c7237531053ccdd
alias-rahil <rahil.kabani.4@gmail.com>
2020-11-19 02:12:51 +0530

    fix: now allow only post requests

--- services/api/api.js
+++ services/api/api.js
@@ -5,6 +5,11 @@ const ApiService = require('moleculer-web')
 module.exports = {
   mixins: [ApiService],
   settings: {
+    routes: [{
+      aliases: {
+        'POST email': 'email.send'
+      }
+    }],
     port: process.env.PORT || '5001',
     cors: {
       origin: ALLOWED_ORIGINS ? ALLOWED_ORIGINS.split(', ') : ''
