c5a79bb07bab9d7fa7ed6bc4cffc65a583fd0935
alias-rahil <rahil.kabani.4@gmail.com>
2020-12-17 01:39:35 +0530

    feat: integrate sendgrid properly

--- services/email/methods/sengrid.js
+++ services/email/methods/sengrid.js
@@ -1,11 +1,15 @@
 const sgMail = require('@sendgrid/mail')
 sgMail.setApiKey(process.env.SENGRID_API_KEY)
 const sender = process.env.SENDER_EMAIL || 'username username@gmail.com'
+const [name, email] = sender.split(' ')
 
 exports.sengrid = (ctx) => {
   try {
     const data = {
-      from: sender,
+      from: {
+        name,
+        email
+      },
       to: ctx.params.to.split(', '),
       subject: ctx.params.subject,
       text: ctx.params.text,
